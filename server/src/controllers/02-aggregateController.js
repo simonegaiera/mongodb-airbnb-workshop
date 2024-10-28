f6fba767695b652f8b22b49984c4d4150c57035b
“Simone <simogaie88@gmail.com>
2024-10-28 11:24:37 -0400

    aggr fix

--- server/src/controllers/02-aggregateController.js
+++ server/src/controllers/02-aggregateController.js
@@ -6,18 +6,21 @@ export async function getPriceStatistics(req, res) {
 
     try {
         const pipeline = [
+            // TODO: Filter documents to include only those that have both beds and price fields
             {
               $match: {
                 beds: { $exists: true },
                 price: { $exists: true }
               }
             },
+            // TODO: Group the documents by the number of beds and calculate the average price for each group
             {
               $group: {
                 _id: '$beds',
                 avgPrice: { $avg: '$price' }
               }
             },
+            // TODO: Sort the grouped documents by the number of beds in ascending order
             { $sort: {_id : 1 } }
           ]        
 
