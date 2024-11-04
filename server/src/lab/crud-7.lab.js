b04155365326561276133a9e34cd17ad05205008
“Simone <simogaie88@gmail.com>
2024-11-04 15:23:36 -0500

    fix

--- server/src/lab/crud-7.lab.js
+++ server/src/lab/crud-7.lab.js
@@ -2,7 +2,6 @@ import { db } from "../utils/database.js";
 import { collectionName } from '../config/config.js';  
 
 export async function crudUpdateElement(id, key, value) {
-
     const search = { _id: id }
     const update = { $set: { [key] : value } }
     
