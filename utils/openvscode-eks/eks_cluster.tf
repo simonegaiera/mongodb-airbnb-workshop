6f680cc9051a3e95764e8a2388fe3bc37fb534a8
“Simone <simogaie88@gmail.com>
2024-12-28 15:40:53 +0100

    upgrade_policy

--- utils/openvscode-eks/eks_cluster.tf
+++ utils/openvscode-eks/eks_cluster.tf
@@ -192,9 +192,9 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
 
-  # upgrade_policy {
-  #   support_type = "STANDARD"
-  # } 
+  upgrade_policy {
+    support_type = "STANDARD"
+  } 
 
   vpc_config {
     endpoint_private_access = true
