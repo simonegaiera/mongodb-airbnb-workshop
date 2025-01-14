c52a5953cef617749c65d90302cdaf207c3435f6
“Simone <simogaie88@gmail.com>
2025-01-14 15:42:04 -0500

    remove old

--- utils/old/docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Nginx in the background
-nginx &
-
-# Start OpenVSCode Server
-su -m openvscode-server -c "exec ${OPENVSCODE} --host 0.0.0.0 --port 8000 --without-connection-token"
