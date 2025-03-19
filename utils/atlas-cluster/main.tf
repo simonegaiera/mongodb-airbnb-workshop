4b22ddef6ebd9467ed88312bde599797d6b58fa3
“Simone <simogaie88@gmail.com>
2025-03-19 16:36:43 -0400

    tf

--- utils/atlas-cluster/main.tf
+++ utils/atlas-cluster/main.tf
@@ -13,6 +13,10 @@ provider "mongodbatlas" {
 
 data "mongodbatlas_roles_org_id" "org" {}
 
+# data "mongodbatlas_project" "project" {
+#   name   = var.project_name
+# }
+
 resource "mongodbatlas_project" "project" {
   name   = var.project_name
   org_id = data.mongodbatlas_roles_org_id.org.org_id
