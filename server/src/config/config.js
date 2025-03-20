f3331c852a4b059b7583289179d9a44c71c55872
“Simone <simogaie88@gmail.com>
2025-03-20 09:44:10 -0400

    config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,11 @@ import '../utils/loadEnvironment.js';
 
 export const port = process.env.PORT || 5000;
 export const mongodbUri = process.env.MONGODB_URI;
-export const databaseName = 'sample_airbnb';
+
+const regex = /mongodb\+srv:\/\/(.*?):.*?@/;
+const match = mongodbUri.match(regex);
+export const databaseName = match ? match[1] : 'sample_airbnb';
+
 export const collectionName = 'listingsAndReviews';
 export const resultsDatabaseName = 'airbnb_gameday';
 export const resultsCollectionName = 'results';
