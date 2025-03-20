dfe297d08d657924abae7760fce0a657d1ebab64
“Simone <simogaie88@gmail.com>
2025-03-20 09:51:40 -0400

    error

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -5,7 +5,7 @@ export const mongodbUri = process.env.MONGODB_URI;
 
 const regex = /mongodb\+srv:\/\/(.*?):.*?@/;
 const match = mongodbUri.match(regex);
-export const databaseName = match ? match[1] : 'sample_airbnb';
+export const databaseName = match ? match[1] : 'error';
 
 export const collectionName = 'listingsAndReviews';
 export const resultsDatabaseName = 'airbnb_gameday';
