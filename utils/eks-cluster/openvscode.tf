4b127ce4959d3920cd14b7f262d6c6b3fe74c848
“Simone <simogaie88@gmail.com>
2025-10-31 15:33:03 -0400

    vscode fix

--- utils/eks-cluster/openvscode.tf
+++ utils/eks-cluster/openvscode.tf
@@ -50,7 +50,7 @@ resource "helm_release" "user_openvscode" {
   name       = substr("vscode-${each.value}", 0, 53)
   repository = "local"
   chart      = "./mdb-openvscode"
-  version    = "0.1.3"
+  version    = "0.1.4"
   timeout    = 600
   
   values = [
